0c3d68a5ef7cec5ca0dd56c7b92caa65f7515ebd
Aryan Verma <aryanverma100999@gmail.com>
2026-06-23 23:09:42 +0530

    Add models and testbench

---
+++ full_model_tb.v
@@ -0,0 +1,98 @@
+`timescale 1ns/1ps
+
+module full_model_tb;
+
+reg pc_out,ce,jump,clk,wr,reset,switch;
+wire [7:0]bus;
+
+reg bus_drive;
+reg [7:0]bus_tb;
+
+    PC_model pc1(.bus(bus),.pc_out(pc_out),.ce(ce),.jump(jump),.clk(clk),.reset(reset));
+    ram_model ram1(.clk(clk),.wr(wr),.bus(bus).switch(switch));
+
+    initial
+        begin
+            clk = 0;
+            forever #5 clk = ~clk;
+        end
+
+    initial
+        begin
+            #1;
+            pc_out = 1'b0;          // pc doesnt pass the value to bus rn
+            ce = 1'b0;
+            jump = 1'b0;
+            reset = 1'b1;
+            wr = 1'b1;
+            bus_drive =1'b1;        //bus is initialised with 8-bits 0;
+            bus_tb = 8'b0000_0000;
+            switch = 1'b0;
+
+            #10;
+
+            reset = 1'b0;
+            pc_out = 1'b1;      //pc passes the 1st address i.e. 0000 to the bus
+            ce = 1'b0;
+            wr = 1'b1;          // wr = 1 means ram is reading this address
+            bus_drive =1'b0;
+
+            #10;
+
+            pc_out = 1'b0;      
+            ce = 1'b0;
+            wr = 1'b0;          // wr = 0 means ram iswriting this address onto bus
+
+            #10;
+
+            pc_out = 1'b1;      
+            ce = 1'b1;
+            wr = 1'b1;          // wr = 0 means ram is reading;
+
+            #10;
+
+            pc_out = 1'b0;      
+            ce = 1'b0;
+            wr = 1'b0;          // wr = 0 means ram is writing this address onto bus
+
+            #10;
+
+            pc_out = 1'b1;      
+            ce = 1'b1;
+            wr = 1'b1;          // wr = 0 means ram is reading;
+
+            #10;
+
+            pc_out = 1'b0;      
+            ce = 1'b0;
+            wr = 1'b0;          // wr = 0 means ram is writing this address onto bus
+
+            #10;
+
+            bus_tb = 8'b0000_0100;
+            bus_drive = 1'b1;
+            wr = 1'b1;          // wr = 1 reading 
+            jump = 1'b1;
+            
+            #10;
+            
+            bus_drive = 1'b0;
+            jump = 1'b0;
+            wr = 1'b0;          // wr = 0 writing
+
+            #10;
+            
+            ce = 1'b1;
+            pc_out = 1'b1;
+            wr = 1'b1;          // reading
+
+            #10;
+
+            ce = 1'b0;
+            pc_out = 1'b0;
+            wr = 1'b1;
+
+            #10;
+
+        end
+endmodule
